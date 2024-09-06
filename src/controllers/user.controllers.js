69c86fb8a9b9f72c6c663966582b54159c76183e
Kul Karki <Kkarki349@rku.ac.in>
2024-09-06 18:27:26 +0530

    refresh and access token implementaion

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import jwt from "jsonwebtoken";
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -144,4 +145,38 @@ const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie("refreshToken")
     .json(new ApiResponse(200, {}, "User logged Out"));
 });
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken;
+  // i think there some mistake 
+  if(incomingRefreshToken) {
+    throw new ApiError(401,"unauthorized request");
+  }
+  try {
+    const decodedToken=jwt.verify(
+      incomingRefreshToken, 
+      process.env.REFRESH_TOKEN_SECRET,
+    )
+    const user=await User.findById(decodedToken?._id);
+    if(!user){
+      throw new ApiError(401,"Invalid refreshTOken");
+    }
+    if(incomingRefreshToken!==user?.refreshToken){
+    throw new ApiError(401,"Refresh Token is expired or used")
+    }
+    const options={
+      httpOnly:true,
+      secure:true
+    }
+   const {accessToken, newRefreshToken}= await generateAccessAndRefreshTokens(user)._id;
+    return res
+    .status(200),
+    cookie(accessToken,"accessToken",options)
+    .cookie(newRefreshToken,"newRefreshToken",options)
+    .json(new ApiResponse(200,{
+      accessToken,refreshToken:newRefreshToken
+    },"Access token refreshed"))
+  } catch (error) {
+    throw new ApiError(401,error?.message|| "Invalid refresh token")
+  }
+})
+export { registerUser, loginUser, logoutUser,refreshAccessToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, registerUser,logoutUser} from "../controllers/user.controllers.js";
+import {loginUser, registerUser,logoutUser,refreshAccessToken} from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verfyJWT } from "../middlewares/auth.middlewares.js";
 const router= Router();
@@ -18,7 +18,8 @@ router.route("/register").post(
     registerUser);
     router.route("/login").post(loginUser);
 // secured route
-router.route("/logout").post(verfyJWT,logoutUser)
+router.route("/logout").post(verfyJWT,logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 
 
